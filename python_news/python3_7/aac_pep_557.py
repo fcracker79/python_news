598eaf2ac48be2f5da9bf5d8828fda9cf660e6d0
Mirko Bonasorte <mirko@conio.com>
2022-10-26 11:51:18 +0200

    default factory in dataclasses

--- python_news/python3_7/aac_pep_557.py
+++ python_news/python3_7/aac_pep_557.py
@@ -1,4 +1,5 @@
 import dataclasses
+import typing
 
 
 @dataclasses.dataclass(
@@ -29,6 +30,23 @@ class FrozenPoint:
     z: float = 0.0
 
 
+@dataclasses.dataclass(frozen=True)
+class ClassWithFields:
+    x: typing.List[str] = dataclasses.field(default_factory=list)
+    y: set = frozenset()
+
+
+@dataclasses.dataclass(frozen=True)
+class TestPostInit:
+    x: typing.List[str]
+    y: typing.Dict[str, int] = None
+
+    def __post_init__(self):
+        object.__setattr__(self, 'y', {s: i for i, s in enumerate(self.x)})
+        # Cannot set on frozen instances
+        # self.y = {s: i for i, s in enumerate(self.x)}
+
+
 if __name__ == '__main__':
     p = Point(1.5, 2.5)
     print(p)  # "Point(x=1.5, y=2.5, z=0.0)"
@@ -37,3 +55,10 @@ if __name__ == '__main__':
     except TypeError as e:
         assert e.args[0] == 'unhashable type: \'Point\''
     {FrozenPoint}
+    cf1 = ClassWithFields()
+    cf2 = ClassWithFields()
+    assert cf1.x is not cf2.x
+    assert cf1.y is cf2.y
+    t = TestPostInit(x=['a', 'b'])
+    assert 0 == t.y['a']
+    assert 1 == t.y['b']
